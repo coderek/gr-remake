3512f39310f7fdf85d808748afbc2b2228fcdf4d
Zeng Qiang <zen1986@gmail.com>
2014-12-15 21:20:52 -0500

    add style to pre element and omit style and class from washer whitelist

--- server/libs/washer.js
+++ server/libs/washer.js
@@ -1,15 +1,15 @@
 var cheerio = require('cheerio');
 var _ = require('lodash');
 
-var exceptions = ['abbr', 'accept', 'accept', -'charset', 'accesskey', 'action',
-    'align', 'alt', 'axis', 'border', 'cellpadding', 'cellspacing', 'char', 'charoff', 'charset',
-    'checked', 'cite', 'class', 'clear', 'cols', 'colspan', 'color', 'compact', 'coords', 'datetime',
-    'dir', 'disabled', 'enctype', 'for', 'frame', 'headers', 'height', 'href', 'hreflang', 'hspace', 'id',
+var exceptions = ['abbr', 'accept', 'accept', 'charset', 'accesskey', 'action',
+    'align', 'alt', 'axis', 'cellpadding', 'cellspacing', 'char', 'charoff', 'charset',
+    'checked', 'cite', 'clear', 'cols', 'colspan', 'color', 'compact', 'coords', 'datetime',
+    'dir', 'disabled', 'enctype', 'for', 'frame', 'headers', 'href', 'hreflang', 'id',
     'ismap', 'label', 'lang', 'longdesc', 'loop', 'loopcount', 'loopend', 'loopstart',
     'maxlength', 'media', 'method', 'multiple', 'name', 'nohref',
     'noshade', 'nowrap', 'poster', 'preload', 'prompt', 'readonly', 'rel', 'rev', 'rows', 'rowspan', 'rules', 'scope',
-    'selected', 'shape', 'size', 'span', 'src', 'start', 'style', 'summary', 'tabindex', 'target', 'title',
-    'type', 'usemap', 'valign', 'value', 'vspace', 'width', 'xml:lang'];
+    'selected', 'shape', 'size', 'span', 'src', 'start', 'summary', 'tabindex', 'target', 'title',
+    'type', 'usemap', 'valign', 'value', 'vspace', 'xml:lang'];
 
 exports.wash = function (content) {
     var $ = cheerio.load(content);
